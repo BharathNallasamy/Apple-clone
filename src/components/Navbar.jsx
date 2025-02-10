6c9d631648b04e3c86e4232d8acd2d30962b063f
Bharath <premkumar@hasnatech.onmicrosoft.com>
2025-02-10 18:23:55 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import {appleImg, bagImg, searchImg} from '../utils';
+import {navLists} from '../constants';
+
+const Navbar = () => {
+  return (
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple" width={18} height={18} />
+
+        <div className="flex justify-center flex-1 max-sm:hidden">
+          {navLists.map (nav => (
+            <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transistion-all">
+              {nav}
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <img src={searchImg} alt="Search" width={18} height={18} />
+          <img src={bagImg} alt="bag" width={18} height={18} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
