0b68bc8a0e72e4cddf374c3e5a368e9da4a26f32
Bharath <premkumar@hasnatech.onmicrosoft.com>
2025-02-10 18:35:15 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Apple-clone", // Give the name of the Repository, then in the package.json -> "homepage": "https://<your-username>.github.io/<repository-name>"
   plugins: [react()],
-})
+});
